5b305f189c6399c77a42e55a9d6d98e0b17309ab
nilaneo <shlikhtenkoneonila@gmail.com>
2018-05-09 17:18:49 +0300

    Routing via dispatch

--- src/products/store/effects/pizzas.effect.ts
+++ src/products/store/effects/pizzas.effect.ts
@@ -4,6 +4,7 @@ import { Effect, Actions } from '@ngrx/effects';
 import { of } from 'rxjs/observable/of';
 import { map, switchMap, catchError } from 'rxjs/operators';
 
+import * as fromRoot from '../../../app/store';
 import * as pizzaActions from '../actions/pizzas.action';
 import * as fromServices from '../../services';
 import { PizzaDisplayComponent } from '../../components';
@@ -41,6 +42,19 @@ export class PizzasEffects {
   );
 
   @Effect()
+  createPizzaSuccess$ = this.actions$
+    .ofType(pizzaActions.CREATE_PIZZA_SUCCESS)
+    .pipe(
+      map((action: pizzaActions.CreatePizzaSuccess) => action.payload),
+      map(
+        pizza =>
+          new fromRoot.Go({
+            path: ['/products', pizza.id]
+          })
+      )
+    );
+
+  @Effect()
   updatePizza$ = this.actions$.ofType(pizzaActions.UPDATE_PIZZA).pipe(
     map((action: pizzaActions.UpdatePizza) => action.payload),
     switchMap(pizza => {
@@ -65,4 +79,19 @@ export class PizzasEffects {
         );
     })
   );
+
+  @Effect()
+  handlePizzaSuccess$ = this.actions$
+    .ofType(
+      pizzaActions.UPDATE_PIZZA_SUCCESS,
+      pizzaActions.REMOVE_PIZZA_SUCCESS
+    )
+    .pipe(
+      map(
+        pizza =>
+          new fromRoot.Go({
+            path: ['/products']
+          })git
+      )
+    );
 }
