393ccca2ed4f6a89a0d9f02dcc3472381cda5570
n.shlikhtenko <n.shlikhtenko@levi9.com>
2018-05-10 16:55:49 +0300

    Add guards that check Store Entities

--- src/products/guards/index.ts
+++ src/products/guards/index.ts
@@ -1,5 +1,9 @@
-import { PizzasGuard } from './pizzas.quard';
+import { PizzasGuard } from './pizzas.guard';
+import { ToppingsGuard } from './toppings.guard';
+import { PizzaExistsGuard } from './pizza-exists.guard';
 
-export const guards: any[] = [PizzasGuard];
+export const guards: any[] = [PizzasGuard, ToppingsGuard, PizzaExistsGuard];
 
-export * from './pizzas.quard';
+export * from './pizzas.guard';
+export * from './toppings.guard';
+export * from './pizza-exists.guard';
